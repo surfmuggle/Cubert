929fa00bdf6483a0c10c70adf2f2aa9a56751769
surfmuggle <1271555+surfmuggle@users.noreply.github.com>
2020-09-24 23:13:50 +0200

    Copied text from srp tutorial

---
+++ javascript/srp-m.js
@@ -0,0 +1,23 @@
+    class Warehouse
+        def calculate_total_assets
+            #...
+            inventory_information = get_tabulated_inventory_info()
+            #...
+        end
+
+        def need_additional_stock?
+            #...
+            inventory_information = get_tabulated_inventory_info()
+            #...
+        end
+
+
+        def save_inventory_records
+            #...
+        end
+
+        private
+        def get_tabulated_inventory_info
+            #...
+        end
+    end
